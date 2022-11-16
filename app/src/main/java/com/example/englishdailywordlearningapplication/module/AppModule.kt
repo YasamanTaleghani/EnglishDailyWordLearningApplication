2cc5c7e559b3cbd97a57010984b28204ec762eb5
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-11-16 09:13:32 +0330

    title: add AppModule.kt note: add required hilt modules as providing database in AppModule.kt

---
+++ app/src/main/java/com/example/englishdailywordlearningapplication/module/AppModule.kt
@@ -0,0 +1,31 @@
+package com.example.englishdailywordlearningapplication.module
+
+import android.content.Context
+import androidx.room.Room
+import com.example.englishdailywordlearningapplication.data.source.local.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideYourDatabase(@ApplicationContext app: Context) =
+        Room.databaseBuilder(app, AppDatabase::class.java, "DemoDatabase.db")
+        .createFromAsset("database/DemoDatabase.db")
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideYourDao(db: AppDatabase) = db.userDao()
+
+    @Provides
+    fun provideIoDispatcher() = Dispatchers.Main
+}
