f2f70c22f900518be6b45efb557457971eacd634
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-11-22 09:47:04 +0330

    title: update MainRepository.kt note: update MainRepository.kt with inheritance from MainRepositoryInterface

--- app/src/main/java/com/example/englishdailywordlearningapplication/data/source/MainRepository.kt
+++ app/src/main/java/com/example/englishdailywordlearningapplication/data/source/MainRepository.kt
@@ -1,14 +1,15 @@
 package com.example.englishdailywordlearningapplication.data.source
 
 import com.example.englishdailywordlearningapplication.data.Word
+import com.example.englishdailywordlearningapplication.data.source.local.MainRepositoryInterface
 import com.example.englishdailywordlearningapplication.data.source.local.WordDao
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class MainRepository @Inject constructor(private val dao: WordDao) {
+class MainRepository @Inject constructor(private val dao: WordDao): MainRepositoryInterface {
 
-    suspend fun getWords(): List<Word> {
+    override suspend fun getWords(): List<Word> {
         return dao.getWords()
     }
 }
