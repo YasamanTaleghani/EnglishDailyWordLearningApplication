5610b14c5f0d74eacde10b14811c9c31460695ba
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-11-27 10:44:29 +0330

    title: update dependency injection module note: update dependency injection contents

--- app/src/main/java/com/example/englishdailywordlearningapplication/module/DataModule.kt
+++ app/src/main/java/com/example/englishdailywordlearningapplication/module/DataModule.kt
@@ -1,21 +1,42 @@
 package com.example.englishdailywordlearningapplication.module
 
+import android.content.Context
+import androidx.room.Room
 import com.example.englishdailywordlearningapplication.data.cache.PreferenceInterface
 import com.example.englishdailywordlearningapplication.data.cache.UserPreference
 import com.example.englishdailywordlearningapplication.data.source.WordDataSource
+import com.example.englishdailywordlearningapplication.data.source.local.AppDatabase
 import com.example.englishdailywordlearningapplication.data.source.local.WordLocalDataSource
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AnalyticsModule {
+abstract class DataModule {
 
     @Binds
     abstract fun provideWordDataSource(wordLocalDataSource: WordLocalDataSource): WordDataSource
 
     @Binds
     abstract fun provideUserPreference(userPreference: UserPreference): PreferenceInterface
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideYourDatabase(@ApplicationContext app: Context) =
+        Room.databaseBuilder(app, AppDatabase::class.java, "DemoDatabase.db")
+            .createFromAsset("test.db")
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideYourDao(db: AppDatabase) = db.userDao()
 }
