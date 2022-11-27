72873610c9de7836bfb12a85d00c75138f0a08fb
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-11-27 10:28:09 +0330

    title: update CoroutineModule.kt note: update dispatcher type from Main to IO in CoroutineModule object

--- app/src/main/java/com/example/englishdailywordlearningapplication/module/CoroutineModule.kt
+++ app/src/main/java/com/example/englishdailywordlearningapplication/module/CoroutineModule.kt
@@ -4,11 +4,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 @Module
 @InstallIn(SingletonComponent::class)
 object CoroutineModule {
     @Provides
-    fun provideIoDispatcher() = Dispatchers.Main
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
