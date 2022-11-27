c720f9faf6b9f9336e14e81c2b00206339113072
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-11-27 10:43:19 +0330

    title: update WordDataRepository.kt note: update getWords method output type and implementation

--- app/src/main/java/com/example/englishdailywordlearningapplication/data/source/WordDataRepository.kt
+++ app/src/main/java/com/example/englishdailywordlearningapplication/data/source/WordDataRepository.kt
@@ -1,15 +1,21 @@
 package com.example.englishdailywordlearningapplication.data.source
 
-import com.example.englishdailywordlearningapplication.data.Word
+import kotlinx.coroutines.*
 import javax.inject.Inject
 import javax.inject.Singleton
+import com.example.englishdailywordlearningapplication.data.Result.Error
+import com.example.englishdailywordlearningapplication.data.Result
+import com.example.englishdailywordlearningapplication.data.Word
 
 @Singleton
-class WordDataRepository @Inject constructor(private val wordLocalDataSource: WordDataSource):
-    WordRepository {
+class WordDataRepository @Inject constructor(private val wordLocalDataSource: WordDataSource,
+                                             private val ioDispatcher: CoroutineDispatcher): WordRepository {
 
-    override suspend fun getWords(): List<Word> {
-        return wordLocalDataSource.getWords()
+    override suspend fun getWords(): Result<List<Word>> = withContext(ioDispatcher) {
+        return@withContext try {
+            wordLocalDataSource.getWords()
+        } catch (e: Exception) {
+            Error(e)
+        }
     }
-
 }
