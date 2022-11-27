0035d33901a2b5c8b4e671bbbd2b185019f512e0
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-11-27 10:42:36 +0330

    title: update WordLocalDataSource.kt note: update getWords method output type and implementation

--- app/src/main/java/com/example/englishdailywordlearningapplication/data/source/local/WordLocalDataSource.kt
+++ app/src/main/java/com/example/englishdailywordlearningapplication/data/source/local/WordLocalDataSource.kt
@@ -1,14 +1,25 @@
 package com.example.englishdailywordlearningapplication.data.source.local
 
+import com.example.englishdailywordlearningapplication.data.Result
+import com.example.englishdailywordlearningapplication.data.Result.Success
+import com.example.englishdailywordlearningapplication.data.Result.Error
 import com.example.englishdailywordlearningapplication.data.Word
 import com.example.englishdailywordlearningapplication.data.source.WordDataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class WordLocalDataSource @Inject constructor(private val dao: WordDao): WordDataSource {
+class WordLocalDataSource @Inject constructor(
+    private val dao: WordDao,
+    private val ioDispatcher: CoroutineDispatcher): WordDataSource {
 
-    override suspend fun getWords(): List<Word> {
-        return dao.getWords()
+    override suspend fun getWords(): Result<List<Word>> = withContext(ioDispatcher) {
+        return@withContext try {
+            Success(dao.getWords())
+        } catch (e: Exception) {
+            Error(e)
+        }
     }
 }
